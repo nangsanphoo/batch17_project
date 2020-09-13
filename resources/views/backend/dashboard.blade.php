becf8db3d72e99a7304078ff9f958288ffb5c76b
thandarswe900 <nannthandarswe@gmail.com>
2020-09-13 11:16:10 +0630

    devide template

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -6,11 +6,11 @@
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
           </div>
 
           <!-- Content Row -->
-          <div class="row">
+          {{-- <div class="row">
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
@@ -295,7 +295,7 @@
               </div>
 
             </div>
-          </div>
+          </div> --}}
 
         </div>
         <!-- /.container-fluid -->

a7f0b8301cc9c25f14c01b13c9e73fd44eade603
thandarswe900 <nannthandarswe@gmail.com>
2020-09-15 18:19:38 +0630

    edit

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relationships
+    public function customer($value='')
+    {
+        return $this->hasOne('App\Customer');
+    }
 }

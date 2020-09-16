7e34a048d9dd1692aa81c0ec590eca92891ee85f
nangphongtip <nangphongtipp@gmail.com>
2020-09-16 11:48:56 +0630

    add link

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,9 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('address');
-            $table->string('birthday');
-            $table->string('photo');
+            
 
             $table->timestamps();
         });
